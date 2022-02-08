3a2a46f517ba4c2e10777e94753ae082cbe4637a
Mercedes <mercedeswagner84@gmail.com>
2022-02-08 15:56:31 +0100

    first push

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+let Vorname = document.getElementById("vorname");
+let Nachname = document.getElementById("nachname");
+let Land = document.getElementById("land");
+
+function yourData() {
+    console.log("test");
+
+    console.log("Full name: " + Vorname.value + " " + Nachname.value + ", Land: " + Land.value);
+}
